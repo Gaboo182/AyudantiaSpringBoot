ccf00926ba861858c892f3b987f63c927fe21c0a
Felipe Vega <felipe.vega.14@sansano.usm.cl>
2019-04-06 00:30:05 -0300

    Finish login

--- src/main/java/com/ayudantia/demoproyecto/Controllers/UserController.java
+++ src/main/java/com/ayudantia/demoproyecto/Controllers/UserController.java
@@ -1,11 +1,16 @@
 package com.ayudantia.demoproyecto.Controllers;
 
+import com.ayudantia.demoproyecto.Models.User;
 import com.ayudantia.demoproyecto.Services.UserService;
 import com.ayudantia.demoproyecto.Services.SecurityService;
+import com.ayudantia.demoproyecto.Validators.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -15,6 +20,28 @@ public class UserController {
     @Autowired
     private SecurityService securityService;
 
+    @Autowired
+    private UserValidator userValidator;
+
+    @GetMapping("/signup")
+    public String signup(Model model){
+        model.addAttribute("userForm", new User());
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@ModelAttribute("userForm") User userForm, BindingResult bindingResult){
+        userValidator.validate(userForm, bindingResult);
+
+        if(bindingResult.hasErrors()){
+            return "signup";
+        }
+
+        userService.save(userForm);
+        securityService.autoLogin(userForm.getUsername(), userForm.getPasswordConfirm());
+
+        return "redirect:/secret";
+    }
 
     @GetMapping("/login")
     public String login(Model model, String error, String logout){
--- src/main/java/com/ayudantia/demoproyecto/Validators/UserValidator.java
+++ src/main/java/com/ayudantia/demoproyecto/Validators/UserValidator.java
@@ -8,6 +8,8 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
+import java.util.regex.Pattern;
+
 
 @Component
 public class UserValidator implements Validator {
@@ -25,7 +27,24 @@ public class UserValidator implements Validator {
 
         ValidationUtils.rejectIfEmptyOrWhitespace(errors,"username","NotEmpty");
         if(user.getUsername().length() <6 || user.getUsername().length()>32){
-            errors.rejectValue("username","Size.userform.username");
+            errors.rejectValue("username","Size.userForm.username");
+        }
+        if(userService.findByUsername(user.getUsername()) != null){
+            errors.rejectValue("username","Duplicate.userForm.username");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"password","NotEmpty");
+        if(user.getPassword().length()<6 || user.getPassword().length()>32){
+            errors.rejectValue("password","Size.userForm.password");
+        }
+        if(!user.getPasswordConfirm().equals(user.getPassword())){
+            errors.rejectValue("passwordConfirm","Diff.userForm.passwordConfirm");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"correo","NotEmpty");
+        Pattern pattern = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",Pattern.CASE_INSENSITIVE);
+        if(!(pattern.matcher(user.getCorreo()).matches())){
+            errors.rejectValue("correo","Inv.userForm.correo");
         }
     }
 }
